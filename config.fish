3acca21688feb3dcfb3f13b2e3a6a32fd9e1ccca
Mark Hansen <mark@markhansen.co.nz>
2012-01-03 18:07:02 +1100

    first commit

---
+++ config.fish
@@ -0,0 +1,20 @@
+function fish_prompt
+    printf '%s@%s%s%s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (git_prompt) (set_color normal)
+end
+
+function git_prompt
+    if git symbolic-ref HEAD >/dev/null ^/dev/null
+        set_color yellow
+        printf ' (git:%s' (git-current-branch)
+        git cherry -v >/dev/null ^/dev/null; and printf '+%d' (git-num-commits-to-push ^/dev/null)
+        printf ')'
+    end
+end
+
+function git-current-branch
+    git symbolic-ref HEAD ^/dev/null | awk -F/ '{print $3;}'
+end
+
+function git-num-commits-to-push
+    git cherry -v | wc -l
+end
---
+++ functions/jsonpp.fish
@@ -0,0 +1,3 @@
+function jsonpp
+	python -mjson.tool $argv; 
+end
