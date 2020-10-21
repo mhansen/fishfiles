4cce975e804b99e0bb3f204d26a47fafb056e05d
Mark Hansen <mark@markhansen.co.nz>
2020-10-21 20:03:10 +1100

    tmux nesty

--- config.fish
+++ config.fish
@@ -1,6 +1,13 @@
-if test $TERM != "screen-256color"
-  tmux attach; or tmux
+# Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
+# only run in interactive (not automated SSH for example)
+if status is-interactive
+# don't nest inside another tmux
+and not set -q TMUX
+  # Adapted from https://unix.stackexchange.com/a/176885/347104
+  # Create session 'main' or attach to 'main' if already exists.
+  tmux new-session -A -s main
 end
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/mark/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/mark/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/mark/Downloads/google-cloud-sdk/path.fish.inc'; end; end
+
