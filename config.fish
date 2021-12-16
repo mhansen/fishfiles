f496cf34dab35176c76248dc054f34e368b5f523
Mark Hansen <mark@markhansen.co.nz>
2021-12-17 08:47:27 +1100

    Use new path to homebrew

--- config.fish
+++ config.fish
@@ -1,3 +1,8 @@
+set -gx PATH /opt/homebrew/bin $PATH
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mark/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/mark/Downloads/google-cloud-sdk/path.fish.inc'; end
+
 # Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
 # only run in interactive (not automated SSH for example)
 if status is-interactive
@@ -7,7 +12,4 @@ and not set -q TMUX
   # Create session 'main' or attach to 'main' if already exists.
   tmux new-session -A -s main
 end
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/mark/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/mark/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/mark/Downloads/google-cloud-sdk/path.fish.inc'; end; end
 
